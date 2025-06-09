3302f1ba9a54aef7a152417d7ef3633847f092e2
Evidence Ejimone <23btrcn075@jainuniversity.ac.in>
2025-06-09 15:08:06 +0530

    feat: initialize frontend project with Expo, add reset script, and configure TypeScript

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+  // there should be nothinn the top of thr screen
+  // the only content should be a text "Hello World"
+  return <Text>Hello World</Text>;
+}
