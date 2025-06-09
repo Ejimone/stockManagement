a59a64be7068a8a649bbf76f495bafb06f30c1da
Evidence Ejimone <23btrcn075@jainuniversity.ac.in>
2025-06-09 18:39:26 +0530

    refactor: simplify layout components and add initial page files

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,7 +1,12 @@
-import { Text } from "react-native";
+import { Text, StatusBar, View } from "react-native";
 
 export default function HomeScreen() {
-  // there should be nothinn the top of thr screen
+  // there should be nothing the top of the screen
   // the only content should be a text "Hello World"
-  return <Text>Hello World</Text>;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <StatusBar hidden={true} />
+      <Text>Hello World</Text>
+    </View>
+  );
 }
